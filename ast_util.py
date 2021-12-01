48d6c19ea706bfafe05a9f41dd1325aa582ffeb5
Nghi D. Q. Bui <bdqnghi@gmail.com>
2021-12-02 00:40:05 +0700

    update

---
+++ ast_util.py
@@ -0,0 +1,52 @@
+import sys
+
+
+class ASTUtil():
+    import logging
+    LOGGER = logging.getLogger('ASTUtil')
+
+    # Simplify the AST 
+    def simplify_ast(self, tree, text):
+        # tree = self.ast_parser.parse(text)
+        root = tree.root_node
+
+        ignore_types = ["\n"]
+        num_nodes = 0
+        root_type = str(root.type)
+        queue = [root]
+
+        root_json = {
+            "node_type": root_type,
+            "node_token": [], # usually root does not contain token
+            "children": []
+        }
+
+        queue_json = [root_json]
+        while queue:
+            
+            current_node = queue.pop(0)
+            current_node_json = queue_json.pop(0)
+            num_nodes += 1
+
+
+            for child in current_node.children:
+                child_type = str(child.type)
+                if child_type not in ignore_types:
+                    queue.append(child)
+
+                    child_token = ""
+                    has_child = len(child.children) > 0
+
+                    if not has_child:
+                        child_token = text[child.start_byte:child.end_byte]
+
+                    child_json = {
+                        "node_type": child_type,
+                        "node_token": child_token,
+                        "children": []
+                    }
+
+                    current_node_json['children'].append(child_json)
+                    queue_json.append(child_json)
+
+        return root_json, num_nodes
---
+++ test_parsing.py
@@ -0,0 +1,41 @@
+from ast_parser import ASTParser
+from ast_util import ASTUtil
+
+def main():
+
+	parser = ASTParser(language="java")
+	ast_util = ASTUtil()
+	sample_code = """
+		public class Main {
+			public static void main(String[] args) {
+				int var1 = 15;
+				int var2 = 20;
+				int var3 = var1 + var2;
+			}
+		}
+		"""
+	tree = parser.parse(str.encode(sample_code))
+	# print the ast object of tree-sitter
+	print(tree)
+
+	"""
+	Some simple traversal on the tree-sitter's ast, refer to https://github.com/tree-sitter/py-tree-sitter
+	for more information
+	"""
+	root_node = tree.root_node
+	print(root_node)
+	class_declaration_node = root_node.children[0]
+	print(class_declaration_node)
+
+	#-----------------------------
+
+	"""
+	simplfy_ast is a function to traverse the ast using Breadth First Traversal to conver the ast into 
+	a simpler format, which is as a Python dictionary.
+	"""
+	simplified_ast = ast_util.simplify_ast(tree, sample_code)
+	print(simplified_ast)
+
+
+if __name__ == "__main__":
+    main()
